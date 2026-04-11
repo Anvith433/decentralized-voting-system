3760b767f6ce70c833b57a01ec92202c160f9726
Anvith433 <anvithshetty932@gmail.com>
2026-04-11 03:32:52 +0100

    Added backend repository layer

---
+++ backend/src/main/java/com/blockchain/voting/repository/AuditLogRepository.java
@@ -0,0 +1,9 @@
+package com.blockchain.voting.repository;
+
+import com.blockchain.voting.model.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+    List<AuditLog> findByVoterId(String voterId);
+}
